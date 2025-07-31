a71bf592ecd1515bd92da2c31b1a2e0b0e22f554
Yasmin Serag <159052828+yasminserag08@users.noreply.github.com>
2025-07-31 20:22:57 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const apiKey = "3a58d57be667e419d11430ebc8ac6896";
+
+async function getForecast(city)
+{
+  const geoUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  const geoRes = await fetch(geoUrl);
+  const geoData = await geoRes.json();
+  const lat = geoData.coord.lat;
+  const lon = geoData.coord.lon;
+  const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  const forecastRes = await fetch(forecastUrl);
+  const forecastData = await forecastRes.json();
+  const forecastDataList = forecastData.list;
+  const forecasts = forecastDataList.map(forecast => ({
+    date: forecast.dt_txt,
+    feels_like: forecast.main.feels_like,
+    humidity: forecast.main.humidity,
+    temp: forecast.main.temp,
+    temp_max: forecast.main.temp_max,
+    temp_min: forecast.main.temp_min
+  }));
+}
