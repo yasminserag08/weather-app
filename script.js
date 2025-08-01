f44308aa03e08e38a770639880639f0fa124291c
Yasmin Serag <159052828+yasminserag08@users.noreply.github.com>
2025-08-01 15:08:13 +0300

    Add location detection

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ const apiKey = "3a58d57be667e419d11430ebc8ac6896";
 const getForecastBtn = document.querySelector('#get-forecast');
 const cityInput = document.querySelector('#search-city');
 
-getForecastBtn.addEventListener('click', async () => {
-  const forecast = await getForecast(cityInput.value)
-  renderForecast(forecast);
+window.onload = function() 
+{
+  getLocation();
+};
+
+getForecastBtn.addEventListener('click', async function(event) {
+  event.preventDefault();
+  const forecasts = await getForecasts(cityInput.value)
+  forecasts.forEach(forecast => {
+    renderForecast(forecast);
+  });
 });
 
-async function getForecast(city)
+async function getForecasts(city)
 {
   const geoUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
   const geoRes = await fetch(geoUrl);
@@ -19,6 +27,7 @@ async function getForecast(city)
   const forecastRes = await fetch(forecastUrl);
   const forecastData = await forecastRes.json();
   const forecastDataList = forecastData.list;
+  console.log(forecastDataList);
   const forecasts = forecastDataList.map(forecast => ({
     date: forecast.dt_txt,
     feels_like: forecast.main.feels_like,
@@ -28,16 +37,50 @@ async function getForecast(city)
     temp_min: forecast.main.temp_min
   }));
   console.log(forecasts);
-  return forecasts[0];
+  return forecasts;
 }
 
 function renderForecast(forecast)
 {
-  document.body.innerHTML += `
+  document.body.innerHTML = `
     <div>
       <ul>
+        <li>Date: ${forecast.date}</li>
         <li>Temperature: ${forecast.temp}</li>
         <li>Feels like: ${forecast.feels_like}</li>  
       </ul>
-    </div>`
+    </div>`;
 }
+
+async function getLocation()
+{
+  if('geolocation' in navigator)
+  {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+  else
+  {
+    document.body.innerHTML = 'Browser does not support geolocation';
+  }
+}
+
+async function success(position)
+{
+  const crd = position.coords;
+  const lat = crd.latitude;
+  const lon = crd.longitude;
+  const geoUrl = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&appid=${apiKey}`
+  const geoRes = await fetch(geoUrl);
+  const geoData = await geoRes.json();
+  const name = geoData[0].name;
+  const state = geoData[0].state;
+  document.body.innerHTML += `
+    <p>${state}</p>
+    <p>${name}</p>`;
+}
+
+function error(err)
+{
+  document.body.innerHTML = 'Location access denied';
+}
+
